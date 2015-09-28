40cdbaa4616e3c378f2db3268d1b5e66a1b6179e
edulis8 <eric_broberg@hotmail.com>
2015-09-28 15:42:29 -0700

    fully working js and all that;

--- app.js
+++ app.js
@@ -1,9 +1,80 @@
 $(document).ready(function() {
+
+
+var params = {
+	client_id: 'ed9a920ebd23472b8733b9b8a754f77a'
+};
+
+var urlBegin = "https://api.instagram.com/v1/tags/";
+var urlEnd= "/media/recent";
+var goldenberries = "goldenberries";
+
+$('li:nth-child(1)').click(function(event) {
+	$('#search-results').empty();
+	showFruit("tamarillo");
+});
+
+
+$('li:nth-child(2)').click(function(event) {
+	$('#search-results').empty();
+	showFruit(goldenberries);
+	showFruit("goldenberry")
+});
+
+$('li:nth-child(3)').click(function(event) {
+	$('#search-results').empty();
+	showFruit("babaco");
+	showFruit("babacopapaya")
 	
+});
 
-	$.getJSON('https://api.instagram.com/v1/tags/goldenberries/media/recent?client_id=ed9a920ebd23472b8733b9b8a754f77a',
-		function(data){console.log(data);
-		});
+$('li:nth-child(4)').click(function(event) {
+	$('#search-results').empty();
+	showFruit("tazziberry");
+	showFruit("tazziberries");
+	showFruit("ugni")
+});
 
+
+
+
+
+
+
+
+
+function showFruit(fruit){
+
+		$.get(urlBegin+fruit+urlEnd, params,
+		function(data){
+			
+
+			for (var i = 0; i < data.data.length; i++) {
+				var image = data.data[i].images.thumbnail.url;
+				var text = data.data[i].caption.text;
+
+				text = shortenText(text);
+
+				$('#search-results').append("<figure><img src="+image+"><figcaption>"+text+"</figcaption></figure>");
+				
+			};
+			
+		}, 'jsonp');
+}
+
+
+
+function shortenText(text){
+	var newStr='';
+
+	for (var i = 0; i < 140 ; i++) {
+		if(text[i]){
+			newStr+= text[i];
+		}
+	};
+
+	return newStr+"...";
+
+}
 
 });
